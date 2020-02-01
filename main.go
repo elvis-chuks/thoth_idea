88dde31d4cdbd44cd9e31776efba797fb58c7db5
elvis-chuks <celvischuks@gmail.com>
2020-02-01 15:54:57 +0100

    added bootstrap

--- main.go
+++ main.go
@@ -165,60 +165,73 @@ func main() {
 	}
 }
 
-const homeHTML = `<!DOCTYPE html>
+const homeHTML = `<!doctype html>
 <html lang="en">
-    <head>
-        <title>Thoth websocket example</title>
-    </head>
-	<body>
-		<div class="wrap">
-			<p><h3>Thoth logs</h3></p>
-			<div class="tool-bar">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-                <div class="download">
-                    <a href="#" download="thoth.log">Download</a>
-                </div>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Thoth Dashboard</title>
+  </head>
+  <body>
+    <div class="wrap">
+        <p><h3>Thoth logs</h3></p>
+        <div class="tool-bar">
+            <div class="download">
+                <a href="localhost:8080/thoth.log" download="thoth.log">Download</a>
             </div>
-			<div class="card">
-				<div class="card-body">
-					<pre id="fileData">{{.Data}}</pre>
-				</div>
-			</div>
-		</div>
-		<script type="text/javascript">
-            (function() {
-				var seckKey = prompt("enter secure key");
-				// prolly use this for authentication
-				console.log(seckKey)
-                var data = document.getElementById("fileData");
-                var conn = new WebSocket("ws://{{.Host}}/ws?lastMod={{.LastMod}}");
-                conn.onclose = function(evt) {
-                    data.textContent = 'Connection closed';
-                }
-                conn.onmessage = function(evt) {
-                    console.log('file updated');
-                    data.textContent = evt.data;
-                }
-            })();
-		</script>
-		
-		<style>
-		
-		.wrap{
-			margin:auto;
-			width:90%;
-		}
-		.card{
-			
-			box-shadow: 2px 2px 5px 2px rgba(53, 196, 60, 0.3);
-			border-radius:10px;
-		}
-        .card-body{
-            padding:20px;
-            background-color:black; 
-			color:green;
-        }
-		</style>
-    </body>
+        </div>
+        <div class="card">
+            <div class="card-body ">
+                <pre id="fileData" class="white-text">{{.Data}}</pre>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript">
+        (function() {
+            // var seckKey = prompt("enter secure key");
+            // // prolly use this for authentication
+            // console.log(seckKey)
+            var data = document.getElementById("fileData");
+            data.style.color = "white"
+            var conn = new WebSocket("ws://{{.Host}}/ws?lastMod={{.LastMod}}");
+            conn.onclose = function(evt) {
+                data.textContent = 'Connection closed';
+            }
+            conn.onmessage = function(evt) {
+                console.log('file updated');
+                data.textContent = evt.data;
+            }
+        })();
+    </script>
+    
+    <style>
+    
+    .wrap{
+        margin:auto;
+        width:90%;
+    }
+    /* .card{
+        box-shadow: 2px 2px 5px 2px rgba(53, 196, 60, 0.3);
+     }  */
+    .card-body{
+        padding:20px;
+        background-color:black; 
+        color:white;
+    } 
+    </style>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
+
+
 `
